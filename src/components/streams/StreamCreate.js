6e121d3c916a618130c357f09ebf04134fd411c8
Grishmin karki <grishmink@gmail.com>
2020-06-08 23:34:51 +0545

    done with formsubmission and validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -12,19 +12,44 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
           component={this.renderInput}
           label="Enter Description"
         />
+        <div style={{ marginTop: "1rem" }}>
+          <button className="ui button primary">Submit</button>
+        </div>
       </form>
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
 //reduxForm is like a connect function but this function only takes single object as an argument
 export default reduxForm({
   form: "streamCreate",
