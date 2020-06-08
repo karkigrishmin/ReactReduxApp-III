7a605d9dfc7177a0f9360988375cb2dcb81a448f
Grishmin karki <grishmink@gmail.com>
2020-06-08 16:35:26 +0545

    wired up input with formProps

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  //destructuring
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+//reduxForm is like a connect function but this function only takes single object as an argument
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
