1b7719a4e7969938d5a615a98b7194857fc8a937
Grishmin karki <grishmink@gmail.com>
2020-06-14 15:12:53 +0545

    added an array of object(i.e. list of streams) in state object using mapKeys function

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -6,10 +6,11 @@ import {
   EDIT_STREAM,
   DELETE_STREAM,
 } from "../actions/types";
-import { act } from "react-dom/test-utils";
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
