8f6eedf54daf62cc87e0352b843b4be31aabd79d
Grishmin karki <grishmink@gmail.com>
2020-06-17 14:26:02 +0545

    created this component for reusable form

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  //destructuring
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    //parent component pass down a callback called onSubmit
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+//this function gets called with all values of inputs
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+//reduxForm is like a connect function but this function only takes single object as an argument
+export default reduxForm({
+  form: "streamForm", //not really required
+  validate: validate,
+})(StreamForm);
+
+//we dont need to call action creator, parent component calls action creator so we remove this code
+// export default connect(null, { createStream })(fromWrapped);
