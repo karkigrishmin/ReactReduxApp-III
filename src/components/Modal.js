25ecfdf9f9d7d69b67cf1a1dc4dcd05d3359474f
Grishmin karki <grishmink@gmail.com>
2020-06-20 00:57:45 +0545

    added event handler to navigate to root route when modal background is clicked

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDom.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
         <div className="header">Delete Stream</div>
         <div className="content">
           Are you sure you want to delete this stream?
